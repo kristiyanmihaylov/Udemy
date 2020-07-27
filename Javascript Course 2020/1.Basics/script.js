ebba593b1906422da93a2804432b4a96a999099f
iamkrismihaylov <kristiyanmihaylov1@gmail.com>
2020-07-27 17:14:16 +0200

    added all material

---
+++ Javascript Course 2020/1.Basics/script.js
@@ -0,0 +1,46 @@
+/* Variables and data Types*/
+
+/*
+var fName = 'John';
+// var = variable
+// variable I assigned the 'string' John
+// '' = ""
+
+var lName = 'Smith';
+var age = 25;
+
+//Data types are 5:
+//1.Number = point numbers
+//2.String = sequence of letters = texr
+//3.Boolean = logical data shows TRUE / FALSE
+//4.Undefined = data which is not existent
+//5.Null = just like 4.
+
+// JS has dynamic typing and auto assign variables
+
+// always give variables meaningful names = age, not a
+//JS uses camelCase notation
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher'
+console.log(job);
+
+var _3years = 3;
+var $3years = 3;
+// when I start a variable with a number =
+// I need to put $ or _ infront of
+
+var num = 23;
+// I cannot use reserve keywords for variables
+
+*/
+/* Variable mutation and type coercion*/
+
+var fName = 'John'; //sting
+var age = 28; //number
+
+console.log(fName+' '+age)
---
+++ Javascript Course 2020/1.Basics/what is JS.js
@@ -0,0 +1,13 @@
+// JS is cross-platform object oriented
+// JS is a core web dev tech
+// JS is client and server side
+// the Course is for Client = WEB side
+// NODE JS is for Server side
+// JS = moderd dynamic web
+// Frameworks are used for fast building
+// they are 100% based on JS = to use them
+// I need to know JS
+
+// HTML is the noun
+// CSS is the adjective
+// JS is the verbs
